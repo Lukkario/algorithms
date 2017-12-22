2c60cff94bd5d8138f53572f2b3daf3dd6573674
Michał Łukiański <michallukianski.0@gmail.com>
2017-12-23 00:14:30 +0100

    Add files via upload

--- convex_hull.py
+++ convex_hull.py
@@ -1,4 +1,8 @@
 import math
+
+def det(p, q , r):
+	return ((p[0]*q[1]) + (q[0]*r[1]) + (r[0]*p[1]) - ((r[0]*q[1]) + (r[1]*p[0]) + (p[1]*q[0])))
+
 def graham_convex_hull(points):
 	O = min(points, key=lambda x: x[1])
 	points = sorted(points, key=lambda x: math.atan2(x[1] - O[1], x[0] - O[0]))
