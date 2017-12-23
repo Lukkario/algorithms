e0b4f41b961f0bf45cc0e2863bd57d95d791052a
Michał Łukiański <michallukianski.0@gmail.com>
2017-12-23 01:00:10 +0100

    Add files via upload
    added Jarvis march algorithm and fixed graham_convex_hull function

--- convex_hull.py
+++ convex_hull.py
@@ -6,10 +6,29 @@ def det(p, q , r):
 def graham_convex_hull(points):
 	O = min(points, key=lambda x: x[1])
 	points = sorted(points, key=lambda x: math.atan2(x[1] - O[1], x[0] - O[0]))
-	punkty.remove(O)
+	points.remove(O)
 	stack = [O]
-	for p in punkty:
+	for p in points:
 		while len(stack)>2 and det(stack[-2], stack[-1], p) < 0:
 			stack.pop()
 		stack.append(p)
 	return stack
+	
+def jarvis_march(points):
+	points = sorted(points, key=lambda x: (x[0], x[1]))
+	startPoint = points[0]
+	start = points[0]
+	points.append(start)
+	end = points[1]
+	hull = [start]
+	while points:
+		for p in points:
+			if det(start, end, p) < 0:
+				end = p
+		hull.append(end)
+		if end == startPoint:
+			break
+		points.remove(end)
+		start = end
+		end = points[0]
+	return hull
